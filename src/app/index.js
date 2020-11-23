ae81d2a05c94eb0967beb448af6680f066c78b6f
Santiago Betancur Graciano <santybg_94@hotmail.com>
2020-11-23 11:55:29 -0500

    Proyecto usando webpack, babel, css y sass

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,5 @@
+import 'bootstrap'
+import './../sass/style.scss'
 import { Propietario, sumaNumeros, datosInmuebles } from './clases/Propietario'
 
 console.log(Propietario)
@@ -5,3 +7,19 @@ console.log(sumaNumeros(2, 5))
 console.log(datosInmuebles)
 console.log("Hola desde Webpack!")
 let a = 7;
+
+let x = 10
+
+let f = { nombre: "oscar" }
+
+const otro = {
+    nombre
+} = f
+
+let numeros = [10, 29, 38]
+
+let suma = (n1, n2) => n1 + n2
+
+let arregloNumeros = numeros.map(numero => numero + 10)
+
+console.log(arregloNumeros)
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,35 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 console.log("Esta es la ruta: ", path.resolve(__dirname, 'build'))
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/app/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'build')
+    },
+    devServer: {
+        port: 7094,
+        open: true
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+
+        }, {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }, {
+            test: /\.s[ac]ss$/i,
+            use: [
+                // Creates `style` nodes from JS strings
+                "style-loader",
+                // Translates CSS into CommonJS
+                "css-loader",
+                // Compiles Sass to CSS
+                "sass-loader",
+            ],
+        }]
     },
     plugins: [
         new HtmlWebpackPlugin({ template: './src/index.html' })
